1b69f321c604b2d05b79605a33848e98c8bd7656
TAG_Venom <19665892+sc0tt88@users.noreply.github.com>
2026-08-13 21:00:17 +0100

    Add transported explosion overrides

--- src/DDraw/ddraw.cpp
+++ src/DDraw/ddraw.cpp
@@ -44,6 +44,7 @@ using namespace std;
 #include "ExplosionCapsTelemetry.h"
 #include "ZeroDamageMapWeapons.h"
 #include "RepairRateFix.h"
+#include "TransportedExplosions.h"
 #ifdef TADR_DEBUG_PIPE
 #include "DebugPipeServer.h"
 #endif
@@ -213,6 +214,7 @@ bool APIENTRY DllMain(HINSTANCE hinst, unsigned long reason, void*)
 		AlliedBuildQueueSync::Install();
 		ChallengeResponse::GetInstance();
 		UnitDefExtensions::GetInstance();
+		TransportedExplosions::Install();
 		CUnitRotate::RegisterUnitDefKeys();
 		CBuildGhost::RegisterUnitDefKeys();
 		// Eager-construct so the DrawGameScreen inline hook is
